01adda94513c34372ddae4a5fb1d82cb854eee16
noir0815 <a4mhkbtb@gmail.com>
2023-11-17 15:09:18 +0900

    laravel-basic-kadaiをコミット

---
+++ laravel-basic-kadai/resources/views/posts/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width" >
+    </head>
+    <body>
+        <h1>投稿一覧</h1>
+    </body>
+</html>
