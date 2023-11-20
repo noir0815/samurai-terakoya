59dd41b9d7dac63fb470572faf0c50285861cf4e
noir0815 <a4mhkbtb@gmail.com>
2023-11-20 13:56:35 +0900

    Eloquent ORMを使ってデータを取得しよう

--- laravel-basic-kadai/app/Http/Controllers/PostController.php
+++ laravel-basic-kadai/app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 class PostController extends Controller{
     public function index(){
@@ -12,4 +13,10 @@ class PostController extends Controller{
 
         return view('posts/index',compact('posts'));
     }
+
+    public function show($id){
+        $post =Post::find($id);
+        return view('posts.show',compact('post'));
+    }
+
 }
